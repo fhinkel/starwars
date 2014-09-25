9e37a9f45af1be8a68710a8e5657c99cf4dec5d6
Clemens Buchacher <drizzd@aon.at>
2014-09-25 21:37:25 +0200

    add polygon

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 var jQuery = require('jquery');
 
+var Polygon = function (origin, angles) {
+    this.origin = origin;
+    this.angles = angles;
+
+    this.draw = function (ctx) {
+        ctx.beginPath();
+        for (var i = 0; i < this.angles.length; i++) {
+            var x = 20 * Math.cos(2 * Math.PI * this.angles[i]);
+            var y = 20 * Math.sin(2 * Math.PI * this.angles[i]);
+            ctx.lineTo(this.origin[0] + x, this.origin[1] + y);
+        }
+        ctx.closePath();
+        ctx.strokeStyle = "#000";
+        ctx.stroke();
+        ctx.fillStyle = "#000";
+        ctx.fill();
+    };
+};
 
 jQuery(function () {
     console.log('WE ARE LOADED!!!!');
@@ -8,6 +26,7 @@ jQuery(function () {
 
     var xSpaceship = 200;
     var ySpaceship = 200;
+    var polygon1 = new Polygon([100, 100], [0, .1, .25, .35, .5, .65, .75, .9]);
 
     var processInput = function () {
         console.log('processing input...');
@@ -35,6 +54,7 @@ jQuery(function () {
     var render = function() {
         clear_screen(ctx);
         paint_spaceship_on_screen(ctx);
+        polygon1.draw(ctx);
     };
 
     var oneTickProcess = function () {
