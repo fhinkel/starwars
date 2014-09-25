a5f32464b917e45241b9fa10a3aec9d4f80bf225
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2014-09-25 20:15:44 +0200

    keine index.js mehr

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ var jQuery = require('jquery');
 
 jQuery(function() {
   console.log('WE ARE LOADED!!!!');
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0,0,150,75);
 });
