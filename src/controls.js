427f95ec5cae0e93f013a3b2e86431d254461c74
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2014-10-02 20:44:16 +0200

    delta for moving

--- src/controls.js
+++ src/controls.js
@@ -3,14 +3,15 @@ var gyro = require('./gyro');
 function startTracking (jQuery, spaceship) {
     console.log('starting to track');
     gyro.startTracking(function (o) {
+        var delta = 200/15/5
         // o.x, o.y, o.z for accelerometer
         // o.alpha, o.beta, o.gamma for gyro
         //console.log(o);
         jQuery("#acc").text("x " + o.x + ", y " + o.y +", z " + o.z);
         jQuery("#gyro").text("alpha " + o.alpha + ", beta " + o.beta);
 
-        spaceship.x = spaceship.x + 200/15 * o.x;
-        spaceship.y = spaceship.y - 200/15 * o.y;
+        spaceship.x = spaceship.x + delta * o.x;
+        spaceship.y = spaceship.y - delta * o.y;
 
     });
 }
